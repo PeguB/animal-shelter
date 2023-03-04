bfbae4926aa43b22cc48525eb4b8959e02b340bb
PeguB <bogdanpegulescu2@gmail.com>
2023-03-04 19:31:12 +0200

    First commit

---
+++ backend/shelter-service/shelter-service/src/test/java/pi/shelterservice/ShelterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.shelterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
