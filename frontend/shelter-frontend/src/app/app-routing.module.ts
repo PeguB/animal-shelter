0a79a424deb28f1dc46f908320196c8889e95cf8
Ioana-Novacovici <ioananovacovici@gmail.com>
2023-03-17 23:31:06 +0200

    added login component and routing to loginComponent

--- frontend/shelter-frontend/src/app/app-routing.module.ts
+++ frontend/shelter-frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AppComponent} from "./app.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {
@@ -9,10 +10,15 @@ const routes: Routes = [
     component: AppComponent
   },
   {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
-  }
+  },
+
 ];
 
 @NgModule({
--- frontend/shelter-frontend/src/app/app.module.ts
+++ frontend/shelter-frontend/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
