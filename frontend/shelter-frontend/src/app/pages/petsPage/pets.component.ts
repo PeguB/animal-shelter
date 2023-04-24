ff26fbcec7a78a5b8855ef3f35db163d8de7091b
Ioana-Novacovici <ioananovacovici@gmail.com>
2023-04-24 23:47:37 +0300

    add filter animals by type

--- frontend/shelter-frontend/src/app/pages/petsPage/pets.component.ts
+++ frontend/shelter-frontend/src/app/pages/petsPage/pets.component.ts
@@ -9,12 +9,15 @@ import {Animal} from "../../_models/animal";
 
 export class PetsComponent implements OnInit {
   public filteredAnimals: Animal[] | undefined;
+  public searchFilteredAnimals: Animal[] | undefined;
+  public typeFilteredAnimals: Animal[] | undefined;
   public ANIMALS: Animal[] = [
     {
       name: 'Cara',
-      age: 11,
-      weight: 5.2,
+      age: 16,
+      weight: 6,
       sex: 'female',
+      description: 'description goes here',
       photoPath: 'https://media.istockphoto.com/id/450709593/photo/pug-sitting-and-panting-1-year-old-isolated-on-white.jpg?s=612x612&w=0&k=20&c=JzZdGVpJQuHzakXi13honoI_UtrvCOaHQhgP-P6IUfY=',
       photoIconPath: 'https://c.files.bbci.co.uk/17444/production/_124800359_gettyimages-817514614.jpg',
       type: 'dog',
@@ -22,36 +25,36 @@ export class PetsComponent implements OnInit {
     {
       name: 'Lucky',
       age: 10,
-      weight: 5.2,
+      weight: 2,
       sex: 'female',
-      description: 'lalsslfdjfvjk',
+      description: 'description goes here',
       photoPath: 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_18/3469748/210430-frankie-al-1227_0.jpg',
       photoIconPath: 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_18/3469748/210430-frankie-al-1227_0.jpg',
-      type: 'cat'
+      type: 'dog'
     },
     {
       name: 'Rhea',
       age: 10,
-      weight: 5.2,
+      weight: 5,
       sex: 'female',
-      description: 'lalsslfdjfvjk',
-      type: 'dog'
+      description: 'description goes here',
+      type: 'cat'
     },
     {
       name: 'Simba',
-      age: 10,
-      weight: 5.2,
+      age: 7,
+      weight: 16,
       sex: 'female',
-      description: 'lalsslfdjfvjk',
+      description: 'description goes here',
       type: 'cat'
     },
     {
       name: 'Ray',
-      age: 10,
-      weight: 5.2,
+      age: 5,
+      weight: 25,
       sex: 'female',
-      description: 'lalsslfdjfvjk',
-      type: 'dog'
+      description: 'description goes here',
+      type: 'cat'
     }
   ];
   private buttonNumber: number = 1;
@@ -62,6 +65,9 @@ export class PetsComponent implements OnInit {
 
   ngOnInit(): void {
     this.filteredAnimals = this.ANIMALS;
+    this.searchFilteredAnimals = this.ANIMALS;
+    this.typeFilteredAnimals = this.ANIMALS;
+    this.buttonNumber = 1;
   }
 
   public selectButton(currentButton: number) {
@@ -72,7 +78,35 @@ export class PetsComponent implements OnInit {
     return this.buttonNumber === currentButton || this.buttonNumber === 0 && currentButton === 1;
   }
 
-  public searchAnimalsByName(name: string): void {
-    this.filteredAnimals = this.ANIMALS.filter(animal => animal.name?.toLowerCase().includes(name.toLowerCase()));
+  public filterAnimalsBySearchName(name: string): void {
+    this.searchFilteredAnimals = this.ANIMALS;
+    if (name) {
+      this.searchFilteredAnimals = this.ANIMALS.filter(animal => animal.name?.toLowerCase().includes(name.toLowerCase()));
+    }
+    this.mergeFilters();
   }
+
+  public filterAnimalsByType(type?: string): void {
+    this.typeFilteredAnimals = this.ANIMALS;
+    switch (type) {
+      case 'all' :
+        this.selectButton(1);
+        break;
+      case 'dog' :
+        this.selectButton(2);
+        break;
+      case 'cat' :
+        this.selectButton(3);
+        break;
+    }
+    if (type && type !== 'all') {
+      this.typeFilteredAnimals = this.ANIMALS.filter(animal => animal.type?.toLowerCase().includes(type.toLowerCase()));
+    }
+    this.mergeFilters();
+  }
+
+  private mergeFilters(): void {
+    this.filteredAnimals = this.searchFilteredAnimals?.filter(animal => this.typeFilteredAnimals?.includes(animal));
+  }
+
 }
