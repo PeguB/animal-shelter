7ea8484abebb17eea716b7058171b0aedf22d228
Ioana-Novacovici <ioananovacovici@gmail.com>
2023-05-21 11:26:17 +0300

    homepage done

--- frontend/shelter-frontend/src/app/pages/homePage/home/home.component.ts
+++ frontend/shelter-frontend/src/app/pages/homePage/home/home.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css', '/css/bootstrap.css', '/css/style.css']
+  styleUrls: ['./home.component.css', '/css/bootstrap.css']
 })
 export class HomeComponent implements OnInit {
 
